88f4e2c2f4026a77ce4576b239146a7f722469f0
dingxing <303159963@qq.com>
2021-12-22 12:06:40 +0800

    [*]增加服务器启动项

--- Unity/Assets/Editor/ServerCommandLineEditor/ServerCommandLineEditor.cs
+++ Unity/Assets/Editor/ServerCommandLineEditor/ServerCommandLineEditor.cs
@@ -10,20 +10,47 @@ namespace ET
 {
     public class ServerCommandLineEditor: EditorWindow
     {
+
+        [MenuItem("Tools/打开服务器选项")]
+        private static void ShowWindow()
+        {
+            GetWindow(typeof (ServerCommandLineEditor));
+        }
+        
         public void OnGUI()
         {
-            if (GUILayout.Button("启动"))
+            GUILayout.BeginVertical();
+
+            if (GUILayout.Button("启动MongoDB数据库"))
             {
-                string arguments = $"";
-                ProcessHelper.Run("App.exe", arguments, "../Bin/");
+                ProcessHelper.Run("mongod", @"--dbpath=db", "../Tools/mongodb/bin/");
+            }
+            
+            if (GUILayout.Button("启动守护进程(Watcher)"))
+            {
+                string arguments = $"--AppType=Watcher --Process=1 --Console=1";
+                ProcessHelper.Run("Server.exe", arguments, "../Bin/");
+            }
+            
+            if (GUILayout.Button("启动机器人"))
+            {
+                string arguments = $"--AppType=Robot --Console=1";
+                ProcessHelper.Run("Robot.exe", arguments, "../Bin/");
+            }
+            
+            if (GUILayout.Button("启动服务器"))
+            {
+                string arguments = $"--AppType=Server --Console=1";
+                ProcessHelper.Run("Server.exe", arguments, "../Bin/");
             }
 
-            if (GUILayout.Button("启动数据库"))
+            if (GUILayout.Button("刷新资源"))
             {
-                ProcessHelper.Run("mongod", @"--dbpath=db", "../Database/bin/");
+                AssetDatabase.Refresh();
             }
-
-            GUILayout.EndHorizontal();
+            
+            //GUIUtility.ExitGUI();
+            GUILayout.EndVertical();
         }
     }
 }
