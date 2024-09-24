4b47203f96428119294122de73cb6e2c40062b51
jessy <303159963@qq.com>
2024-09-24 16:44:04 +0800

    使用client模式

--- Unity/Assets/Scripts/Hotfix/Share/ConstValue.cs
+++ Unity/Assets/Scripts/Hotfix/Share/ConstValue.cs
@@ -2,8 +2,8 @@ namespace ET
 {
     public static partial class ConstValue
     {
-        public const string RouterHttpHost = "127.0.0.1";
-        public const int RouterHttpPort = 30300;
+        public const string RouterHttpHost = "47.74.57.149";
+        public const int RouterHttpPort = 80;
         public const int SessionTimeoutTime = 30 * 1000;
         
         
